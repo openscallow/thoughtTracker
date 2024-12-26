61c1ef4c8ef1a4d3ba57c144a1c7c6cc6c831927
Gautam <kaal2045@gmail.com>
2024-12-26 16:53:52 +0530

    incorporate tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		port:3000
+	},
+	resolve:{
+		alias:{
+			'@tailwind':path.resolve('./src/app.css')
+		}
 	}
 });
